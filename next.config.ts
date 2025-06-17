6771a7182c06d7dd2f3716ee3318e40265263080
Jonathan Alejandro Colin Medina <jonyacolin@gmail.com>
2025-06-17 14:07:43 -0600

    Prueba de despliegue 1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: `/my-profile`,
+  assetPrefix: `/my-profile/`,
 };
 
 export default nextConfig;
