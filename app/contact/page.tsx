28deef70fec1a2bce52e61beed73440b26c6e7c7
Jonathan Alejandro Colin Medina <jonyacolin@gmail.com>
2025-06-17 14:58:13 -0600

    Solucionando problema de imagen

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@
 
 import { IconMediaLink } from 'jacm-library';
 import facebook from '../assets/images/facebook.png';
-import instagram from '../assets/images/instagram.png';
+import instagram from '../assets/images/Instagram.png';
 import linkedin from '../assets/images/linkedin.png';
 import gmail from '../assets/images/gmail.png';
 import styles from './page.module.css';
